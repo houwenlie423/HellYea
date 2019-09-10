52c2f24d3be2b4425b4300a12863369aea625a4e
phazeless <32062035+phazeless@users.noreply.github.com>
2019-09-10 11:32:19 +0700

    DOSA

---
+++ Assets/Script/Dosa.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class Dosa 
+{
+    public string NamaDosa;
+    public int Rating;
+}
