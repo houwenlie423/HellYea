140e88c3c7f2016e3f69f08eb404b656d0e63415
phazeless <32062035+phazeless@users.noreply.github.com>
2019-10-08 16:54:38 +0700

    ea

---
+++ Assets/Script/Animationmenu.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Animationmenu : MonoBehaviour
+{
+    public GameObject m_Object;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void f_TurnOff() {
+        this.gameObject.SetActive(false);
+    }
+    public void f_Test() {
+
+        m_Object.SetActive(true);
+    }
+}
